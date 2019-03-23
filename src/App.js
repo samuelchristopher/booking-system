6971585ae13493e7ea357d41b6e078502e21098c
Samuel Paul Christopher <thesamchris@gmail.com>
2019-03-23 12:23:04 +0800

    markdown file data extracted and added to firebase

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import { Router, Link } from 'react-static'
 //
 import Routes from 'react-static-routes'
 
-import './app.css'
+import './styles/main.css'
+import './styles/base.css'
 
 export default () => (
   <Router>
--- static.config.js
+++ static.config.js
@@ -1,6 +1,71 @@
 import React from 'react'
 import axios from 'axios'
 
+// for reading markdown
+import fs from 'fs'
+import vfile from 'to-vfile'
+import report from 'vfile-reporter'
+import unified from 'unified'
+import parse from 'remark-parse'
+import stringify from 'remark-stringify'
+import frontmatter from 'remark-frontmatter'
+import matter from 'gray-matter'
+
+import * as firebase from 'firebase'
+import 'firebase/database'
+
+let config = {
+  apiKey: "AIzaSyB1x4Xulggn_lzxi1JEveDF7jjohoU_wFs",
+  authDomain: "gtabook.firebaseapp.com",
+  databaseURL: "https://gtabook.firebaseio.com",
+  projectId: "gtabook",
+  storageBucket: "gtabook.appspot.com",
+  messagingSenderId: "242321135368"
+}
+
+firebase.initializeApp(config)
+
+
+const readFiles = (dirname, onFileContent, onError) => {
+  fs.readdir(dirname, function (err, filenames) {
+    if (err) {
+      onError(err);
+      return;
+    }
+    filenames.forEach(function (filename) {
+      fs.readFile(dirname + filename, 'utf-8', function (err, content) {
+        if (err) {
+          onError(err);
+          return;
+        }
+        onFileContent(filename, content);
+      });
+    });
+  });
+}
+
+readFiles('content/tours/', function (filename, content) {
+  console.log(filename)
+  unified()
+    .use(parse)
+    .use(stringify)
+    .use(frontmatter, ['yaml', 'toml'])
+    .process(vfile.readSync(`./content/tours/${filename}`), function (err, file) {
+      if (err) {
+        return console.error(report(err))
+      }
+      let { data: tourData } = matter(String(file))
+      return firebase.database().ref(`${tourData.path}`).set({
+        ...tourData
+      });
+      
+    })
+}, function (err) {
+  throw err;
+});
+
+
+
 export default {
   getRoutes: async () => {
     const { data: posts } = await axios.get('https://jsonplaceholder.typicode.com/posts')
