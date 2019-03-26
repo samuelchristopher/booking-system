ecbb612e31e4d3c215e0527de64a8508431c01aa
Samuel Paul Christopher <thesamchris@gmail.com>
2019-03-26 10:38:46 +0800

    fix nav css

--- src/components/nav.js
+++ src/components/nav.js
@@ -5,7 +5,7 @@ import "./nav.css"
 const Nav = () => {
     return (
         <div className="mobile-nav">
-            <a className={styles.logo} style={{ margin: '0 auto' }} href="/" />
+            <a className="logo" style={{ margin: '0 auto' }} href="/" />
             {/* <a className={styles.menuButton} href="#" /> */}
         </div>
         // <div className="mobile-nav">
