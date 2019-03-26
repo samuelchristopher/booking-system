af1b8c634a75612b430e12eed240708cd74d17b9
Samuel Paul Christopher <thesamchris@gmail.com>
2019-03-26 10:31:17 +0800

    temp nav

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,12 +1,16 @@
 import React from "react"
-import "./nav.css"
+import styles from "./nav.module.css"
 
 const Nav = () => {
     return (
         <div className="mobile-nav">
-            <a className="logo" href="/" />
-            <a className="menu-button" href="#" />
+            <a className={styles.logo} style={{ margin: '0 auto' }} href="/" />
+            {/* <a className={styles.menuButton} href="#" /> */}
         </div>
+        // <div className="mobile-nav">
+        //   <a className={styles.logo} href="/" />
+        //   <a className={styles.menuButton} href="#" />
+        // </div>
     )
 }
 
